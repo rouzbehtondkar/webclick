3d2e4f3fdc02ab35e1baefa78d89ddeca572da69
rouzbehtondkar <tondkar.eng@gmail.com>
2023-10-04 19:58:21 +0330

    update again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import Slider from  '@/components/home/Slider'
+import Services from '@/components/Services'
 
 
 export default function Home() {
   return (
     <div className='flex flex-col gap-[20px]'>
 <Slider/>
+<Services/>
     </div>
 
   )
---
+++ src/components/Services.tsx
@@ -0,0 +1,12 @@
+import { useGetAllProductsQuery } from "@/stores/appslice";
+function Services() {
+    const {data}=useGetAllProductsQuery()
+    return (
+        <div>
+            Enter
+            
+        </div>
+    );
+}
+
+export default Services;
