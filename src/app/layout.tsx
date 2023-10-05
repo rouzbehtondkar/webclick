7247433ff4f232537a99227f37f0e4222a8e703d
pooria Vakili <pooriavakili09@gmail.com>
2023-10-05 22:40:10 +0330

    updatecode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '../assets/css/iransans.css'
 import "../assets/styles/modules/tailwind.css"
-
+import "./globals.css"
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -21,7 +21,7 @@ export default function RootLayout({
     <>
    
       <html lang="en">
-      <body >{children}</body>
+      <body>{children}</body>
     </html>
     </>
   
--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -8,7 +8,7 @@ function Header(){
 return(
     <div style={{direction:"rtl"}}  className="flex px-20   justify-between">
     <div>
-  
+  1
     </div>
     <div>02</div>
     <div>03</div>
