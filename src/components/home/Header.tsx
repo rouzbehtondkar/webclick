84cbfd8abf4b10abc3cd0b146e4a0137546f3a79
pooria Vakili <pooriavakili09@gmail.com>
2023-10-06 21:09:57 +0330

    updateheader

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -2,8 +2,8 @@
 'use client';
 
 import { useTranslation } from "next-i18next"
-
 import { Button, Navbar } from 'flowbite-react';
+
 import Link from 'next/link'
 
 function Header(){
@@ -11,53 +11,69 @@ function Header(){
 
 return(
   <>
-   <Navbar
+<div className="hidden 2xl:flex xl:flex  justify-around  mt-5">
+  <div><img src="/img/webclick.png"/></div>
+  <div className="gap-6 flex">
+    <Link href="/">Home</Link>
+    <Link href="/">services</Link>
+    <Link href="/">ourProject</Link>
+    <Link href="/">Aboutus</Link>
+    <div className="2xl:-mt-2 xl:-mt-2 lg:-mt-2 mt-0">
+    <select>
+      <option>en</option>
+      <option>fa</option>
+    </select>
+    </div>
+   
+  </div>
+  <div>
+  <Link href="/" className="bg-blue-500 w-[135px] 2xl:-mt-4 xl:-mt-4 lg:-mt-4 mt-0 h-[52px] rounded-[12px] flex justify-center items-center text-white ">contactus</Link>
+
+  </div>
+</div>
+<div className="block 2xl:hidden xl:hidden lg:hidden">
+<Navbar
       fluid
       rounded
-      className="pb-[96px] pt-[48px]"
     >
-      <Navbar.Brand href="https://flowbite-react.com" 
-      className="flex items-center">
+      <Navbar.Brand >
         <img
           alt="Flowbite React Logo"
           className="mr-3 h-6 sm:h-9"
           src="/img/webclick.png"
         />
-      
+       
       </Navbar.Brand>
-     
+      <div className="flex md:order-2">
+      <Link href="/" className="bg-blue-500 w-[135px] h-[52px] rounded-[12px] flex justify-center items-center text-white ">contactus</Link>
+
+        <Navbar.Toggle />
+      </div>
       <Navbar.Collapse>
         <Navbar.Link
           active
           href="#"
-          className="hover:text-primary"
         >
           <p>
             Home
           </p>
         </Navbar.Link>
-        <Navbar.Link href="#" className="hover:text-primary">
-          Services
+        <Navbar.Link href="#">
+        services
         </Navbar.Link>
-        <Navbar.Link href="#" className="hover:text-primary">
-          OurProject
+        <Navbar.Link href="#">
+        ourProject
         </Navbar.Link>
-        <Navbar.Link href="#" className="hover:text-primary">
-          Aboutus
+        <Navbar.Link href="#">
+        Aboutus
         </Navbar.Link>
-       <select className="-mt-2">
-        <option>en</option>
-        <option>fa</option>
-       </select>
+        <select>
+      <option>en</option>
+      <option>fa</option>
+    </select>
       </Navbar.Collapse>
-      <div className="flex justify-between items-center ">
-        <Link href="/" className="2xl:-mt-2 4xl:mt-5 5xl:mt-5 rounded-[12px] text-white pt-[12px] pb-[12px] pr-[24px] pl-[24px]
-         bg-primary xs:mt-5 xl:-mt-2 md:mt-0 sm:mt-5 lg:-mt-2">
-         contactus
-        </Link>
-        <Navbar.Toggle className="ml-10 mt-5 4xl:mt-5 5xl:mt-5 xs:mt-5"/>
-      </div>
     </Navbar>
+</div>
   </>
     
 )
