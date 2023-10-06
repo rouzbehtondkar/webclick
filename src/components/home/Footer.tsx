411cd696a78d0f4c680c2ad8643a62c39b0db144
pooria Vakili <pooriavakili09@gmail.com>
2023-10-06 22:00:54 +0330

    updatecode

--- src/components/home/Footer.tsx
+++ src/components/home/Footer.tsx
@@ -4,73 +4,36 @@ import { Footer } from 'flowbite-react';
 import {AiOutlineInstagram,AiFillLinkedin,AiOutlineFacebook} from "react-icons/ai"
 export default function FooterSitemapLinks() {
   return (
-    <Footer className='bg-white'>
-      <div className="w-full">
-        <div className="grid w-full grid-cols-2 gap-8 px-6 py-8 md:grid-cols-4">
-          <div>
-            <Footer.LinkGroup col>
-              <Footer.Link href="#">
-                <img src="/img/webclick.png" alt="" />
-              </Footer.Link>
-              <Footer.Link href="#">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ratione explicabo at nobis commodi amet, voluptate quas molestiae architecto pariatur consequatur a quibusdam, esse quos culpa dignissimos aliquid saepe totam.
-              </Footer.Link>
+    <div style={{backgroundColor:"#F5F5F5"}} className=''>
+<div className='2xl:flex xl:flex lg:block justify-around '>
+  <div className='flex flex-col'>
+    <div><img src='/img/webclick.png'/></div>
+    <div>Lorem ipsum, dolor  magni omnis quas id</div>
+  </div>
+  <div className='flex flex-col'>
+    <div>Quick access</div>
+    <div>Home</div>
+    <div>Services</div>
+    <div>Ourproject</div>
+    <div>Aboutus</div>
+  </div>
+  <div className='flex flex-col'>
+    <div>meetus</div>
+    <div>09376720694</div>
+    <div>pooriavakili09@gmail.com</div>
+  </div>
+  <div className='flex flex-col'>
+    <div>followus</div>
+    <div className='flex'>
+      <div><AiOutlineInstagram/></div>
+      <div><AiFillLinkedin/></div>
+      <div><AiOutlineFacebook/></div>
+    </div>
+    
+  </div>
+  </div>
+    </div>
 
-            </Footer.LinkGroup>
-          </div>
-          <div>
-            <Footer.Title title="queekas" />
-            <Footer.LinkGroup col>
-              <Footer.Link href="#">
-Home
-              </Footer.Link>
-              <Footer.Link href="#">
-                Services
-              </Footer.Link>
-              <Footer.Link href="#">
-                OurProject
-              </Footer.Link>
-              <Footer.Link href="#">
-                Aboutus
-              </Footer.Link>
-            </Footer.LinkGroup>
-          </div>
-          <div>
-            <Footer.Title title="meetus" />
-            <Footer.LinkGroup col>
-              <Footer.Link href="#">
-                09376720694
-              </Footer.Link>
-              <Footer.Link href="#">
-                pooriavakili09@gmail.com
-              </Footer.Link>
-             
-            </Footer.LinkGroup>
-          </div>
-          <div>
-            <Footer.Title title="followus" />
-            <div className='flex'>
-            <Footer.LinkGroup>
-              <Footer.Link href="#">
-                <AiOutlineInstagram style={{fontSize:"25px"}}/>
-              </Footer.Link>
-              <Footer.Link href="#">
-                <AiFillLinkedin style={{fontSize:"25px"}}/>
-              </Footer.Link>
-              <Footer.Link href="#">
-               <AiOutlineFacebook style={{fontSize:"25px"}}/>
-              </Footer.Link>
-              
-            </Footer.LinkGroup>
-            </div>
-            
-          </div>
-        </div>
-        <div className="w-full p-2 flex bg-white  justify-center items-center">
-          @2023 webclick,all rights Recerved
-          
-        </div>
-      </div>
-    </Footer>
+
   )
 }
